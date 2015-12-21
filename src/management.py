67a4c26227eab9824d84d992b0ee7a94e297e439
boevering <bart.oevering@student.hu.nl>
2015-12-21 13:52:00 +0100

    XML aangemaakt en alle opties aangezet. en XML werkend gemaakt

--- src/management.py
+++ src/management.py
@@ -1,57 +1,72 @@
 from pysimplesoap.client import SoapClient, SoapFault
 from lxml import etree
 
-# get server from xmlfile
-tree = etree.parse('servers.xml')
-servers = tree.xpath('/servers/server')
+def valueToGet(client):
 
-for i in servers:
-    print "Elk Client: \n       ",etree.tostring(i)
+    # call a few remote methods
+    r1=str(client.get_value(number=1).resultaat)
+    print "sys.platform =1 :", r1
 
-# create a simple consumer
-client = SoapClient(
-    location = "http://localhost:8008/",
-    action = 'http://localhost:8008/', # SOAPAction
-    namespace = "http://example.com/sample.wsdl",
-    soap_ns='soap',
-    ns = False)
+    r2=str(client.get_value(number=2).resultaat)
+    print "sys.getdefaultencoding() =2 :", r2
 
-# call a few remote methods
-r1=str(client.get_value(number=1).resultaat)
-print "sys.platform =1 :", r1
+    r3=str(client.get_value(number=3).resultaat)
+    print "Doet helemaal niks =3 :", int(r3) # r3 is a number!
 
-r2=str(client.get_value(number=2).resultaat)
-print "sys.getdefaultencoding() =2 :", r2
+    r4=str(client.get_value(number=4).resultaat)
+    print "Aantal processen =4 :", r4.rstrip() # This is a multiline: strip the newline from the result!
 
-r3=str(client.get_value(number=3).resultaat)
-print "Doet helemaal niks =3 :", int(r3) # r3 is a number!
+    r5=str(client.get_value(number=5).resultaat)
+    print "Aantal services =5 :", r5.rstrip()
 
-r4=str(client.get_value(number=4).resultaat)
-print "Aantal processen =4 :", r4.rstrip() # This is a multiline: strip the newline from the result!
+    r5=str(client.get_value(number=5).resultaat)
+    print "free_memory =5 :", r5.rstrip()
 
-r5=str(client.get_value(number=5).resultaat)
-print "Aantal services =5 :", r5.rstrip()
+    r6=str(client.get_value(number=6).resultaat)
+    print "free_space =6 :", r6.rstrip()
 
-r5=str(client.get_value(number=5).resultaat)
-print "free_memory =5 :", r5.rstrip()
+    r7=str(client.get_value(number=7).resultaat)
+    print "first_ip =7 :", r7.rstrip()
 
-r6=str(client.get_value(number=6).resultaat)
-print "free_space =6 :", r6.rstrip()
+    r8=str(client.get_value(number=8).resultaat)
+    print "uptime =8 :", r8.rstrip()
 
-r7=str(client.get_value(number=7).resultaat)
-print "first_ip =7 :", r7.rstrip()
+    r9=str(client.get_value(number=9).resultaat)
+    lijst = r9.split(';')
+    print "psutil.disk_usage('c:\\') =9 :", lijst
 
-r8=str(client.get_value(number=8).resultaat)
-print "uptime =8 :", r8.rstrip()
+    r10=str(client.get_value(number=10).resultaat)
+    lijst = r10.split(';')
+    print "psutil.cpu_times() =10 :", lijst
 
-r9=str(client.get_value(number=9).resultaat)
-lijst = r9.split(';')
-print "psutil.disk_usage('c:\\') =9 :", lijst
+    r11=str(client.get_value(number=11).resultaat)
+    lijst = r11.split(';')
+    print "psutil.virtual_memory() =11 :", lijst
 
-r10=str(client.get_value(number=10).resultaat)
-lijst = r10.split(';')
-print "psutil.cpu_times() =10 :", lijst
 
-r11=str(client.get_value(number=11).resultaat)
-lijst = r11.split(';')
-print "psutil.virtual_memory() =11 :", lijst
+def getClientsIP():
+    # get server from xmlfile
+    xmlFile = 'servers.xml'
+    serverPath = '/data/servers/server'
+
+    tree = etree.parse(xmlFile)
+    servers = tree.xpath(serverPath)
+
+    ## count the amount of servers in the file so we know how often we need to ask divertent servers.
+    count = int(tree.xpath('count(//server)'))
+
+    for i in range(count):
+        serverIPAdress =  servers[i][0].text
+        serverIPPort = servers[i][1].text
+
+        # create a simple consumer
+        client = SoapClient(
+            location = "http://" + serverIPAdress + ":" + serverIPPort + "/",
+            action = 'http://localhost:8008/', # SOAPAction
+            namespace = "http://example.com/sample.wsdl",
+            soap_ns='soap',
+            ns = False)
+
+        valueToGet(client)
+
+getClientsIP()
