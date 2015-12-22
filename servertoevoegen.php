2ab73030497780d7b7ae3d7588f197994e5a4831
MikeSlotboom <mike.slotboom@student.hu.nl>
2015-12-22 22:31:32 +0100

    Drop Down Selection

--- servertoevoegen.php
+++ servertoevoegen.php
@@ -29,7 +29,17 @@ else {
 <tr><td> IP adres: </td><td> <input type="text" name="IPAdres" value="" /> </td></tr>
 <tr><td> IP Port: </td><td> <input type="text" name="IPPort" value="" /> </td></tr>
 <tr><td> MAC Adres: </td><td> <input type="text" name="MACAdres" value=""/> </td></tr>
-<tr><td> Operating System: </td><td> <input type="text" name="OperatingSystem" value="" /> </td></tr>
+<tr><td> Operating System: </td><td>
+<select name="OperatingSystem">
+  <option value="win32">Windows 10</option>
+  <option value="win32">Windows 8.1</option>
+  <option value="win32">Windows 8</option>
+  <option value="win32">Windows 7</option>
+  <option value="win32">Windows XP</option>
+  <option value="linux2">Ubuntu</option>
+  <option value="linux2">Debian</option>
+  <option value="linux2">Other</option>
+</select> </td></tr>
 <tr><td> <input type="submit" value="Toevoegen" name="knop" /> </td></tr>
 </form>
 <table>
