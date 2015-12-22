30249a14c349a5ca7c9f2e8d9de18f837e22ffc4
MikeSlotboom <mike.slotboom@student.hu.nl>
2015-12-22 21:53:09 +0100

    XML PHP

--- XMLCreate.php
+++ XMLCreate.php
@@ -12,39 +12,45 @@ $xml->startDocument();
 $xml->setIndent(true);
 
 $xml->startElement('data');
-$xml->startElement('servers');
-while ($row = mysql_fetch_assoc($res)) {
-	$xml->startElement("server");
+	$xml->startElement('servers');
+	while ($row = mysql_fetch_assoc($res)) {
+		$xml->startElement("server");
+			$xml->writeAttribute('name', $row['Name']);
+			
+			$xml->startElement("sID");
+				$xml->writeRaw($row['sID']);
+			$xml->endElement();
+			
+			$xml->startElement("IPAdres");
+				$xml->writeRaw($row['IPAdres']);
+			$xml->endElement();
+			
+			$xml->startElement("IPPort");
+				$xml->writeRaw($row['IPPort']);
+			$xml->endElement();
+	  
+		$xml->endElement();
+	}
+	$xml->endElement();
 
-	$xml->startElement("sID");
-	$xml->writeRaw($row['sID']);
+	$xml->startElement('database');
+		$xml->startElement("dbIPAdres");
+			$xml->writeRaw('10.0.0.14');
+		$xml->endElement();
+		
+		$xml->startElement("dbUser");
+			$xml->writeRaw('monitor');
+		$xml->endElement();
+		
+		$xml->startElement("dbPassword");
+			$xml->writeRaw('raspberry');
+		$xml->endElement();
+		
+		$xml->startElement("dbDatabase");
+			$xml->writeRaw('Monitor');
+		$xml->endElement();
+	  
 	$xml->endElement();
-	$xml->startElement("IPAdres");
-	$xml->writeRaw($row['IPAdres']);
-	$xml->endElement();
-	$xml->startElement("IPPort");
-	$xml->writeRaw($row['IPPort']);
-	$xml->endElement();
-  
-	$xml->endElement();
-}
-$xml->endElement();
-
-$xml->startElement('database');
-	$xml->startElement("dbIPAdres");
-	$xml->writeRaw('10.0.0.14');
-	$xml->endElement();
-	$xml->startElement("dbUser");
-	$xml->writeRaw('monitor');
-	$xml->endElement();
-	$xml->startElement("dbPassword");
-	$xml->writeRaw('raspberry');
-	$xml->endElement();
-	$xml->startElement("dbDatabase");
-	$xml->writeRaw('Monitor');
-	$xml->endElement();
-  
-$xml->endElement();
 $xml->endElement();
 
 header('Content-type: text/xml');
