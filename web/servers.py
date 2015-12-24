b86cd6b81f4d5f7625cfe707efe979e5d1f0069a
boevering <bart.oevering@student.hu.nl>
2015-12-24 14:10:57 +0100

    update naar juiste bestands locatie.

--- web/servers.py
+++ web/servers.py
@@ -109,7 +109,7 @@ if ((knop == "edit") or (knop == "toevoegen")):
     <tr><td> Name: </td><td> <input type="text" name="Name" value="'''+str(row[5])+'''"/> </td></tr>
     <tr><td> IP adres: </td><td> <input type="text" name="IPAdres" value="'''+str(row[1])+'''" /> </td></tr>
     <tr><td> IP Port: </td><td> <input type="text" name="IPPort" value="'''+str(row[2])+'''" /> </td></tr>
-    <tr><td> MAC Adres: </td><td> <input type="text" name="MACAdres" value="'''+str(row[3])+'''"/> </td></tr>
+    <tr><td> MAC Adres: </td><td> <input type="text" name="MACAdres" value=""/> </td></tr>
     <tr><td> Operating System: </td><td>
     <select name="OperatingSystem">
           <option value="win32">Windows 10</option>
