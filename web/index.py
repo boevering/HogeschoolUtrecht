27efa6d8b36110a59cd019331b63dfec20ca42f8
Mike <mike.slotboom@student.hu.nl>
2015-12-25 20:35:24 +0100

    Werken aan index.py

--- web/index.py
+++ web/index.py
@@ -41,7 +41,7 @@ if not knop:
 
     for x in xrange(0,nrrow):
         row = cur.fetchone()
-        print '<form action="" method="post"><input type="submit" value="' + str(row[1][x]) + '" name="knop" /></form></td></tr>'
+        print '<form action="" method="post"><input type="submit" value="' + str(row[1][x]) + '" name="' + str(row[1][x]) + '" /></form>'
 
     print '<table border="1">'
     print '<th>lID</th><th>sID</th><th>TimeStamp</th><th>r1</th><th>r2</th><th>r3</th><th>r4</th><th>r5</th><th>r6</th><th>r7</th><th>r8</th><th>r9</th><th>r10</th><th>r11</th>'
@@ -58,7 +58,10 @@ if not knop:
         print '<td>'+ str(row[8]) + '</td>'
         print '<td>'+ str(row[9]) + '</td>'
         print '<td>'+ str(row[10]) + '</td>'
-        print '<td>'+ str(row[11]) + '</td></tr>'
+        print '<td>'+ str(row[11]) + '</td>'
+        print '<td>'+ str(row[12]) + '</td>'
+        print '<td>'+ str(row[13]) + '</td>'
+        print '<td>'+ str(row[14]) + '</td></tr>'
     # print '<form action="" method="post"><input type="hidden" name="sID" value="' + str(row[1]) + '"  /><input type="submit" value="edit" name="knop" /></form></td>'
     print '</table>'
 
