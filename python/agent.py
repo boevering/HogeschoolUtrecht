14694249dd7e88bceb08d2e548d0dcfc08abdb3a
boevering <bart.oevering@student.hu.nl>
2016-01-09 17:15:29 +0100

    Toevoegen van graph

--- python/agent.py
+++ python/agent.py
@@ -102,7 +102,7 @@ def get_value(number):
         if BS == "win32":
             return getPowerShell('Get-CountPS')
         else:
-            p=subprocess.Popen(['sudo',                 # Altijd gelijk of volledig pad naar powershell.exe
+            p=subprocess.Popen([                 # Altijd gelijk of volledig pad naar powershell.exe
                     'ps -aux', '|' , 'wc -l'],    # Naam van en pad naar je PowerShell script
                 stdout = subprocess.PIPE)               # Zorg ervoor dat je de STDOUT kan opvragen.
             output = p.stdout.read()                    # De stdout
