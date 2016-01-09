ccd6c5e78ef8a23ecb8a87c433c9e54897c1e1a0
MikeSlotboom <mike.slotboom@student.hu.nl>
2016-01-09 13:25:36 +0100

    graph

--- python/graph.py
+++ python/graph.py
@@ -1,7 +1,6 @@
 import pymysql
 import pandas as pd
-import plotly.plotly as py
-from plotly.graph_objs import *
+import matplotlib.pyplot as plt
 from lxml import etree
 
 
@@ -13,37 +12,45 @@ tree = etree.parse(xmlFile)
 database = tree.xpath(databasePath)
 
 conn = pymysql.connect(host=database[0][0].text, user=database[0][1].text, passwd=database[0][2].text, db=database[0][3].text)
-cursor = conn.cursor()
-cursor.execute = "SELECT r4 FROM Logs WHERE sID='1'"
-rows = cursor.fetchall()
+conn.autocommit(True)
+cur = conn.cursor()
+cur.execute("SELECT lID,sID,TimeStamp,r4 FROM Monitor.Logs WHERE sID='1';")
+rows = cur.fetchall()
+cur.close()
 
 
 df = pd.DataFrame( [[ij for ij in i] for i in rows] )
 df.rename(columns={0: 'lID', 1: 'sID', 2: 'TimeStamp', 3: 'r4'}, inplace=True);
-df = df.sort(['lID'], ascending=[1]);
-
+df = df.sort_values(['lID'], ascending=[1]);
 df.head()
 
-sID = df['sID']
-for i in range(len(sID)):
-    try:
-        sID[i] = str(sID[i]).decode('utf-8')
-    except:
-        sID[i] = 'Country name decode error'
+df.plot()
+plt.show()
 
-trace1 = kde(
-    x=df['TimeStamp'],
-    y=df['r4'],
-    text=sID,
-    mode='markers'
-)
-layout = Layout(
-    title='Active Processes',
-    xaxis=XAxis( type='time', title='TimeStamp' ),
-    yaxis=YAxis( title='Processes' ),
-)
-data = Data([trace1])
-fig = Figure(data=data, layout=layout)
-py.iplot(fig, filename='world GNP vs life expectancy')
+# ts = Series(randn(1000), index=date_range('1/1/2000', periods=1000))
+# ts = ts.cumsum()
+# ts.plot()
+#
+# sID = df['sID']
+# for i in range(len(sID)):
+#     try:
+#         sID[i] = str(sID[i]).decode('utf-8')
+#     except:
+#         sID[i] = 'Country name decode error'
+#
+# trace1 = kde(
+#     x=df['TimeStamp'],
+#     y=df['r4'],
+#     text=sID,
+#     mode='markers'
+# )
+# layout = Layout(
+#     title='Active Processes',
+#     xaxis=XAxis( type='time', title='TimeStamp' ),
+#     yaxis=YAxis( title='Processes' ),
+# )
+# data = Data([trace1])
+# fig = Figure(data=data, layout=layout)
+# py.iplot(fig, filename='world GNP vs life expectancy')
 
 # Gebruikt: http://moderndata.plot.ly/graph-data-from-mysql-database-in-python/ en http://nbviewer.ipython.org/gist/jackparmer/5485807511a58be48bf2
