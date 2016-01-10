4005de5e0e59fbf498edd5643a47679f4df6fa9d
boevering <bart.oevering@student.hu.nl>
2016-01-10 10:29:59 +0100

    update van gb naar mb

--- web/index.py
+++ web/index.py
@@ -48,7 +48,7 @@ print '</form>\n'
 
 if (knop):
     if knop == "Alle Servers":
-        sql = "SELECT * FROM Server order by sID;"
+        sql = "SELECT * FROM logs order by sID;"
     else:
         sql = "SELECT * FROM Logs WHERE sID ="+str(knop)+";"
     cur.execute(sql)
