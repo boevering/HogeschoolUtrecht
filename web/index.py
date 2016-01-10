37cb826ad89a54be8a46ff0e34d0880af6fd3d25
MikeSlotboom <mike.slotboom@student.hu.nl>
2016-01-10 12:14:37 +0100

    knoppen aanpassingen

--- web/index.py
+++ web/index.py
@@ -42,7 +42,8 @@ nrrow1= cur.rowcount
 
 print '<a href="servers.py"><input type="submit" value="Server Management" name="knop" /></a>'
 print '<a href="error.py"><input type="submit" value="Error Logs" name="knop" /></a>'
-print '<form action="" method="post"><input type="submit" value="All Servers" name="knop" />'
+print '<a href="index.py"><input type="submit" value="Monitor" name="knop" /></a>'
+print '</ br><form action="" method="post"><input type="submit" value="All Servers" name="knop" />'
 for x in xrange(0,nrrow1):
     row = cur.fetchone()
     print '<input type="submit" value="' + str(row[0]) + '" name="knop" />'
