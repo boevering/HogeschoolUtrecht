4dcdd02ca26b017330a02b85e884d21a952be983
boevering <bart.oevering@student.hu.nl>
2016-01-11 13:13:30 +0100

    Update on graph

--- web/index.py
+++ web/index.py
@@ -65,9 +65,9 @@ if (knop):
 
     if knop.isdigit():
         print '<br /><br />'
-        print '<img src="/images/proc_server'+str(knop)+'.png" />'
-        print '<img src="/images/ram_server'+str(knop)+'.png" />'
-        print '<img src="/images/disk_server'+str(knop)+'.png" />'
+        print '<a href="/images/proc_server'+str(knop)+'.png" target="_blank"><img src="/images/proc_server'+str(knop)+'.png" /></a>'
+        print '<a href="/images/ram_server'+str(knop)+'.png" target="_blank"><img src="/images/ram_server'+str(knop)+'.png" /></a>'
+        print '<a href="/images/disk_server'+str(knop)+'.png" target="_blank"><img src="/images/disk_server'+str(knop)+'.png" /></a>'
         print '<br />'
 print '<br />'
 print '<table border="1">'
