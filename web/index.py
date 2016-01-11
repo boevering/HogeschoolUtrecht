987c06c9aff77aacc174d8424e97494e6839413c
boevering <bart.oevering@student.hu.nl>
2016-01-11 15:14:48 +0100

    Update on graph

--- web/index.py
+++ web/index.py
@@ -83,9 +83,9 @@ if (knop):
 
         memory = current[7].split(';')
         print memory
-        mem = int(memory[2])
-        mem /= int(memory[0])
-        mem *= 100
+        mem = int(memory[0])
+        mem /= int(memory[1])
+        # mem *= 100
         # mem = str(((int(memory[0])/(int(memory[2])))*100)
 
         harddisk = current[8].split(';')
