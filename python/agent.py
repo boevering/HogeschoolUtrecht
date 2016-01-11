1a0cfb39e5145083ad81780d1e3c442896de8d48
boevering <bart.oevering@student.hu.nl>
2016-01-11 14:01:02 +0100

    update on cpu%

--- python/agent.py
+++ python/agent.py
@@ -133,7 +133,7 @@ def get_value(number):
             return socket.gethostbyname(socket.gethostname())
 
     if number == 8:
-        p = psutil.cpu_times()
+        p = psutil.cpu_times_percent()
         waardes = str(p.user) + ';' + str(p.system)  + ';' + str(p.idle)
         return waardes
 
