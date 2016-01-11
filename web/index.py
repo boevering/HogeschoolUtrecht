b8e2efad4dbff2bcd35444d3bd4fff2274989eb9
MikeSlotboom <mike.slotboom@student.hu.nl>
2016-01-11 13:43:55 +0100

    css

--- web/index.py
+++ web/index.py
@@ -88,7 +88,12 @@ for x in xrange(0,nrrow):
     print '<td>'+ str(row[10]) + '</td>'
     print '<td>'+ str(row[11]) + '</td></tr>'
 print '</table>'
-conn.close()
-print '<div><a href="#"><input type="submit" value="Terug Naar Boven" name="knop" class="to-top"/></a></div>'
+print '<div><input type="submit" value="Database Wissen" name="knop" />< /br><a href="#"><input type="submit" value="Terug Naar Boven" name="knop"/></a></div>'
+
+if knop == "Database Wissen":
+    sql = "TRUNCATE `Monitor`.`logs`;"
+    cur.execute(sql)
+
 print("</body>")
 print("</html>")
+conn.close()
